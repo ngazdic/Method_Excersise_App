df7a4f9016dab50f85818a34b25958e58c4ee7e8
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-01 16:15:52 -0400

    changes made according to the excersise one

---
+++ Method_Excersise/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("What is your name?"); //output
+  string name = Console.ReadLine(); //input
+
+Console.WriteLine("Whats your age?");
+  int num = Convert.ToInt32(Console.ReadLine()); // reading integer from console
+
+Console.WriteLine("Cool? - true or false:");
+  bool coolness = Convert.ToBoolean(Console.ReadLine());
+
+Console.WriteLine("Whats your hobby?");
+    string hobby = Console.ReadLine();
+
+
+
+
